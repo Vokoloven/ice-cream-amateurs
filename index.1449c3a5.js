4a8a87e7d78f3cd65afd4d746afee60784f20d83
PolinaIgrushkina <108811462+PolinaIgrushkina@users.noreply.github.com>
2022-08-07 20:29:28 +0000

    Deploying to gh-pages from @ Vokoloven/ice-cream-amateurs@5f12eb172d88b034384426660c607128cdfbe162 🚀

---
+++ index.1449c3a5.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("gallery__backdrop--hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.1449c3a5.js.map
