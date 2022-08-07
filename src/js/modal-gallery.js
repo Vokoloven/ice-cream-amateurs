eb610c15dd2bde613f90144753fd9c4e3a227f0e
Ruslan Volovenko <vokoloven@gmail.com>
2022-08-08 00:01:48 +0300

    feature/gallery

--- src/js/modal-gallery.js
+++ src/js/modal-gallery.js
@@ -1,16 +1,33 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openGalleryBtn: document.querySelector('[data-gallery-open]'),
+    closeGalleryBtn: document.querySelector('[data-gallery-close]'),
+    gallery: document.querySelector('[data-gallery]'),
     body: document.querySelector('body'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openGalleryBtn.addEventListener('click', toggleGallery);
+  refs.closeGalleryBtn.addEventListener('click', toggleGallery);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('gallery__backdrop--hidden');
+  function toggleGallery() {
+    refs.gallery.classList.toggle('gallery__backdrop--hidden');
     refs.body.classList.toggle('no-scroll');
   }
 })();
+
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector('.open-btn'),
+//     closeMenuBtn: document.querySelector('.close-btn'),
+//     menu: document.querySelector('.mob-menu'),
+//     body: document.querySelector('body'),
+//   };
+
+//   refs.openMenuBtn.addEventListener('click', toggleMenu);
+//   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+//   function toggleMenu() {
+//     refs.menu.classList.toggle('is-hidden');
+//     refs.body.classList.toggle('no-scroll');
+//   }
+// })();
