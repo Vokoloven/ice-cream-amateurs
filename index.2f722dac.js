329652402c74c9e3fbfdc981568a108ca25c4508
Vokoloven <48805990+Vokoloven@users.noreply.github.com>
2022-08-09 09:04:12 +0000

    Deploying to gh-pages from @ Vokoloven/ice-cream-amateurs@3f1c549c235e744a7a57b21fca8281f65543c268 🚀

---
+++ index.2f722dac.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");document.querySelector("body"),e.addEventListener("click",(function(){var o="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!o),t.classList.toggle("is-open"),refs.body.classList.toggle("no-scroll")}))}();
+//# sourceMappingURL=index.2f722dac.js.map
