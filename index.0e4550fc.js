d14615276fc303753175211a80111e53ea9ec858
Vokoloven <48805990+Vokoloven@users.noreply.github.com>
2022-08-09 09:03:54 +0000

    Deploying to gh-pages from @ Vokoloven/ice-cream-amateurs@e51b9e51ad0cda07e2668b3623afc25b9877236b 🚀

--- index.0e4550fc.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={openModalBtn:document.querySelector("[data-contacts-open]"),closeModalBtn:document.querySelector("[data-contacts-close]"),modal:document.querySelector("[data-contacts]")};function e(){t.modal.classList.toggle("is-hidden")}t.openModalBtn.addEventListener("click",e),t.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.0e4550fc.js.map
---
+++ index.373b3888.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-contacts-open]"),closeModalBtn:document.querySelector("[data-contacts-close]"),modal:document.querySelector("[data-contacts]"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.373b3888.js.map
